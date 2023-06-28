03c6fcef65d2b51005784cfe91deefb47b0ff2e5
emily <emilybiecoski@icloud.com>
2023-06-28 13:56:12 -0300

    segundo commit

--- main.py
+++ main.py
@@ -11,4 +11,15 @@ pygame.display.set_caption("Space Marker")
 # Definir o ícone da janela
 icone = pygame.image.load("space.png")
 pygame.display.set_icon(icone)
+fundo = pygame.image.load("bg.jpg")
+pygame.mixer.music.load("space_machine.mp3")
+pygame.mixer.music.play(-1)
 
+estrelas = []  # Lista para armazenar as estrelas
+linhas = []  # Lista para armazenar as linhas
+
+instrucoes = [
+    "Pressione F10 para Salvar os Pontos",
+    "Pressione F11 para Carregar os Pontos",
+    "Pressione F12 para Deletar os Pontos"
+]
